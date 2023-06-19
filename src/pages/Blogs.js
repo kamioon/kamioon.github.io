1a09c3a0c0e4bc516ba1d545ab8eac1d24ba2b82
Kamran Azari <kamran.azari@gmail.com>
2023-06-19 11:45:48 +0200

    Very early version

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,12 +1,25 @@
+import React from "react";
+import {Helmet} from "react-helmet";
 
-const Blogs = () => {
-    return (
-        <div id="blog" className="blog">
-            <p>
-                There is no posts available.
-            </p>
-        </div>
-    )
-};
+class Blogs extends React.Component {
+    render() {
+        return (
+            <div id="blog" className="blog">
+
+                <Helmet>
+                    <title>Kamran Azari - Blog Posts</title>
+                    <meta
+                        name="description"
+                        content="latest blog posts of me"
+                    />
+                    ;
+                </Helmet>
+                <p>
+                    There are no posts available :(
+                </p>
+            </div>
+        )
+    }
+}
 
 export default Blogs;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,31 @@
 import React from "react"
 import {Link} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 class Home extends React.Component {
-    componentDidMount() {
-        document.title = "Kamran Azari - Welcome"
-    }
-
     render() {
         return (
             <div className='home' id='home'>
+                <Helmet>
+                    <title>Kamran Azari</title>
+                    <meta
+                        name="description"
+                        content="This is a personal website. content about web development and programming or my experiences found in it."
+                    />;
+                </Helmet>
                 <h1>Kamran Azari</h1>
-                <p>I'm a Software Developer Based in the Netherlands! <Link to="about">read more..</Link></p>
-                <p>This is my personal website/blog and I've just created to share my thought and ideas with
-                    you. [here] you can find me and contact me.
+                <p>I'm a Software Developer Based in the Netherlands! [<Link to="about">read more</Link>]</p>
+                <p>This is my personal website/blog; I've just created it to share my thoughts and ideas with
+                    you. you can find me and contact me [<Link to="contact">Here</Link>].
                 </p>
                 <hr/>
-                <h2>Latest posts</h2>
-                <div className='row post-list'>
-                    <div>P1</div>
-                </div>
-                <div className='row post-list'>
-                    <div>P2</div>
-                </div>
+                {/*<h2>Latest posts</h2>*/}
+                {/*<div className='row post-list'>*/}
+                {/*    <div>P1</div>*/}
+                {/*</div>*/}
+                {/*<div className='row post-list'>*/}
+                {/*    <div>P2</div>*/}
+                {/*</div>*/}
             </div>
         )
     }
